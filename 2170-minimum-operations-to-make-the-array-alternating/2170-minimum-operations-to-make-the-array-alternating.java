c728847be6f1203edb037d84c4ac8c47de68f109
bhumi0987 <bhumibhardwaj267@gmail.com>
2026-02-14 13:33:22 +0530

    Time: 63 ms (73.01%), Space: 109.9 MB (43.25%) - LeetHub

---
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,Integer> odd=new HashMap<>();
+        HashMap<Integer,Integer> even=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                even.put(nums[i], even.getOrDefault(nums[i],0)+1);
+            }
+            else{
+                odd.put(nums[i], odd.getOrDefault(nums[i],0)+1);
+            }
+        }
+        int evenMaxVal=0;
+        int evenMax=0;
+        int evenSecond=0;
+        int oddMaxVal=0;
+        int oddMax=0;
+        int oddSecond=0;
+        for(Map.Entry<Integer,Integer> e:even.entrySet()){
+            int freq=e.getValue();
+            if(freq>evenMax){
+                evenSecond=evenMax;
+                evenMax=freq;
+                evenMaxVal=e.getKey();
+            }else if(freq>evenSecond){
+                evenSecond=freq;
+            }
+        }
+        for(Map.Entry<Integer,Integer> e:odd.entrySet()){
+            int freq=e.getValue();
+            if(freq>oddMax){
+                oddSecond=oddMax;
+                oddMax=freq;
+                oddMaxVal=e.getKey();
+            }else if(freq>oddSecond){
+                oddSecond=freq;
+            }
+        }
+        if(evenMaxVal!=oddMaxVal){
+            return n-evenMax-oddMax;
+        }
+        return n-Math.max(evenMax+oddSecond,oddMax+evenSecond);
+    }
+}
