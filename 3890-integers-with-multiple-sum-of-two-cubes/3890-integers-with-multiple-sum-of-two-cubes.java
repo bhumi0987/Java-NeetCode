e4d4b86c4a55f488fb1f2b6550a93584f55e90f7
bhumi0987 <bhumibhardwaj267@gmail.com>
2026-04-05 10:39:31 +0530

    Time: 313 ms (40.35%), Space: 73.4 MB (98.84%) - LeetHub

--- 3890-integers-with-multiple-sum-of-two-cubes/3890-integers-with-multiple-sum-of-two-cubes.java
+++ 3890-integers-with-multiple-sum-of-two-cubes/3890-integers-with-multiple-sum-of-two-cubes.java
@@ -1,35 +1,33 @@
 class Solution {
-    static boolean computed = false;
-    static List<Integer> v = new ArrayList<>();
-
-    public void compute() {
-        Map<Integer, Integer> map = new TreeMap<>();
-
-        for (int i = 1; i <= 1000; i++) {
-            for (int j = i + 1; j <= 1000; j++) {
-                int sum = i * i * i + j * j * j;
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
+    static List<Integer> pre=new ArrayList<>();
+    static boolean flag=false;
+    public void solve(){
+        Map<Integer,Integer> map=new TreeMap<>();
+        for(int i=1;i<=1000;i++){
+            for(int j=i+1;j<=1000;j++){
+                int sum=i*i*i+j*j*j;
+                map.put(sum,map.getOrDefault(sum,0)+1);
             }
         }
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                v.add(entry.getKey());
+        for(Map.Entry<Integer,Integer> it:map.entrySet()){
+            if(it.getValue()>1){
+                pre.add(it.getKey());
             }
         }
-
-        computed = true;
+        flag=true;
     }
-
     public List<Integer> findGoodIntegers(int n) {
-        if (!computed) compute();
-
-        List<Integer> ans = new ArrayList<>();
-        for (int num : v) {
-            if (num <= n) ans.add(num);
-            else break;
+        if(!flag){
+            solve();
         }
-
+        List<Integer> ans=new ArrayList<>();
+        for(int it:pre){
+            if(it<=n){
+                ans.add(it);
+            }else{
+                break;
+            }
+        }
         return ans;
     }
 }
